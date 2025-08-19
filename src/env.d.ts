71f8f2712286cedf0ef5ee8ada8ae7eef5a78757
eapepe0 <eapepe0@gmail.com>
2025-08-18 23:38:47 -0300

    . Manejo de sesion desde el middleware y isLoggedIn . Paso de middleware a locals para usar el user en toda la aplicacion. . Cerrar sesion con signOut

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 interface User {
   name: string;
   email: string;
-  // TODO:
+  // TODO: expandirlo
 }
 
 declare namespace App {
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,24 @@ import { getSession } from 'auth-astro/server';
 const notAuthenticatedRoutes = ['/login', '/register'];
 
 export const onRequest = defineMiddleware(
-  async ({ url, locals, redirect }, next) => {
-    const isLoggedIn = false;
+  async ({ url, locals, redirect , request }, next) => {
+    const session = await getSession(request); /* sacamos la sesion del request */
+    const isLoggedIn = !!session; /* si existe sesion es que estamos logeados  */
+    const user = session?.user; /* metemos en user , el user de session */
 
     // TODO:
-    locals.isLoggedIn = isLoggedIn;
-    locals.user = null;
+    locals.isLoggedIn = isLoggedIn; /* decimos que isLoggedIn en locals es igual a isLoggedIn de session */
+    locals.user = null; /* creamos en locals un usuario vacio o nulo */
 
-    if (locals.user) {
-      // TODO:
-      // locals.user = {
-      //   avatar: UserActivation.photoURL ?? '',
-      //   email: user.email!,
-      //   name: user.name!,
-      //   emailVerified: user.emailVerified,
-      // };
+    if (isLoggedIn) {
+     
+      /* creamos un objeto en locals con estos datos */
+      locals.user = {
+         /* avatar: UserActivation.photoURL ?? '', */
+         email: user?.email!,
+         name: user?.name!,
+         /* emailVerified: user.emailVerified, */
+      };
     }
 
     // TODO: Eventualmente tenemos que controlar el acceso por roles
