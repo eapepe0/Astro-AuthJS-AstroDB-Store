a06a0a1c6b3c0830d6f78f956c8061c36a107753
eapepe0 <eapepe0@gmail.com>
2025-09-09 17:14:35 -0300

    . Hacemos una consulta al db para que traiga los productos del carro con su foto. . Buscamos entre esos productos de la DB los de nuestro carro . Creamos el array de productos que tienen el productId, title , size , quantity, price , slug

--- src/actions/cart/load-products-from-cart.action.ts
+++ src/actions/cart/load-products-from-cart.action.ts
@@ -3,6 +3,7 @@
 
 import type { CartItem } from "@/interfaces/cart-item";
 import { defineAction } from "astro:actions";
+import { db, eq, inArray, Product, ProductImage } from "astro:db";
 import { z } from 'astro:schema';
 
 // siempre exportamos y definimos la accion
@@ -13,10 +14,37 @@ export const loadProductsFromCart = defineAction({
     handler: async (_,{cookies}) => {
         // si existe la cookie cart sino como un array vacio
         const cart = JSON.parse(cookies.get('cart')?.value ?? '[]') as CartItem[];
-        console.log(cart)
         if(cart.length === 0) return [];
-        return {
-            products :cart
-        };// retorna esto
+
+        /* Carga de productos */
+        const productsIds = cart.map(item => item.productId); // obtenemos los ids de los productos
+
+        const dbProducts = await db
+        .select() // trae todas las columnas seleccionables
+        .from(Product) // de Producto
+        .innerJoin(ProductImage , eq(Product.id , ProductImage.productId)) // se une con ProductImage , donde el id del Producto sea igual al id del ProductImage
+        .where(inArray(Product.id , productsIds));  // filtra los id de Product en los id del carro
+
+        return  cart.map(item => {
+                    const dbProduct = dbProducts.find(p => p.Product.id === item.productId);
+                    if (!dbProduct){
+                        throw new Error(`Product with id ${item.productId} not found`);
+                    }
+                const {title , price , slug} = dbProduct.Product;
+                const image = dbProduct.ProductImage.image;
+
+                return {
+                    productId: item.productId,
+                    title: title,
+                    size : item.size,
+                    quantity: item.quantity,
+                    image : image.startsWith('http')
+                    ? image
+                    : `${import.meta.env.PUBLIC_URL}/images/products/${image}`,
+                    price : price,
+                    slug : slug,
+                }
+            })
+        
     },
 });
