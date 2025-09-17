fd5f83b5dff8f445e151170636deabc2b06be604
eapepe0 <eapepe0@gmail.com>
2025-09-17 12:24:42 -0300

    . Nuestra imagen queda subida en resp.secure_url, usaremos los batch para que los querys siempre se completen. . Haremos con una promesa la subida de las imagenes y cuando se completen todas , se guardaran las secureUrl en un array . Creamos un imgObj , con un id , el imgUrl y el productId, todo esto lo enviaremos a un query para subir todas las querys   al db en forma de lote.

--- src/actions/products/create-update-product.action.ts
+++ src/actions/products/create-update-product.action.ts
@@ -1,6 +1,6 @@
 import { ImageUpload } from "@/utils/image-upload";
 import { defineAction } from "astro:actions";
-import { db, eq, Product } from "astro:db";
+import { db, eq, Product, ProductImage } from "astro:db";
 import { z } from 'astro:schema';
 import { getSession } from "auth-astro/server";
 import {v4 as UUID} from 'uuid'
@@ -60,24 +60,51 @@ export const createUpdateProduct = defineAction({
             ...rest,
         };
 
-        console.log(product)
-
+        const queries : any = []; // creamos una cola
+        
         // si el form.id no existe o es undefined , por que es un producto nuevo , no se genero el producto , es del form no el id = UUID
         if(!form.id ){
-            await db.insert(Product).values(product);  // insertamos el producto en la db
-        } else{ // si ya existe el producto
-            await db.update(Product).set(product).where(eq(Product.id, id)); // actualizamos
+            queries.push(db.insert(Product).values(product));  // insertamos el producto en la db
+        } else { // si ya existe el producto
+            queries.push(db.update(Product).set(product).where(eq(Product.id, id))); // actualizamos
         }
         
         // Imagenes
-        console.log("🚀 ~ :73 ~ imageFiles:", {imageFiles})
+        const secureUrls : string[] = [] // creamos un array de string donde se guardara el resultado de la subida de los archivos , la url de la imagen en cloudinary
+        // si existen las imagenes en el form , si el tamaño total es mayor a 0 y si el tamaño de la primera imagen es mayor a 0
+        if( form.imageFiles && form.imageFiles.length > 0 && form.imageFiles[0].size > 0){
+
+            const urls = await Promise.all( // creamos una promesa y hasta que no se cumplan todas , no se generan en la url
+                form.imageFiles.map( file => ImageUpload.uploadImage(file)) // mapeamos los imageFiles y los vamos subiendo
+            )
+            secureUrls.push(...urls) // guardamos las urls generadas en secureUrls
+        }
+
+        secureUrls.forEach(imageUrl =>{
+            // creamos el objeto imagen para insertar en la db de cada url que exista
+            const imgObj = {
+                id : UUID(),
+                image: imageUrl,
+                productId : product.id
+            }
+            console.log("🚀 ~ :90 ~ imgObj:", imgObj)
+            
+            // creamos el query de insertar el ProductImage y los valores
+            queries.push(db.insert(ProductImage).values(imgObj));
+        })
+
+
 
         // por cada imagen en imageFiles por si es un array
-        imageFiles?.forEach( async imageFile => {
+      /*   imageFiles?.forEach( async imageFile => {
             if(imageFile.size <= 0) return; // si es 0 no hacemos nada
 
             await ImageUpload.uploadImage(imageFile); // subimos la foto
-        })
+        }) */
+
+        
+        // enviamos las queries al db en forma de lote
+        await db.batch(queries);
         return product; // retorna esto
     },
 });
--- src/utils/image-upload.ts
+++ src/utils/image-upload.ts
@@ -12,14 +12,14 @@ cloudinary.config({
 export class ImageUpload{
 
     static async uploadImage(file : File){       // recibimos un File de un input
+
         const buffer = await file.arrayBuffer(); //  convertimos el file a un array binario
         const base64Image = Buffer.from(buffer).toString('base64'); // creamos un Buffer de Node con ese buffer y lo pasamos a base64
         const imageType = file.type.split('/')[1] // image/png  extraemos el png
-        const resp = await cloudinary.uploader.upload(`data:image/${imageType};base64,${base64Image}`,{folder : 'AstroStore'});
 
-        console.log(resp); 
-
-        return 'http://cristianoyola.com.ar'
+        const resp = await cloudinary.uploader.upload(`data:image/${imageType};base64,${base64Image}`,{folder : 'AstroStore'}); // enviamos los datos a cloudinary
+        
+        return resp.secure_url // devolvemos la url de la o las imagenes subidas
     }
 
 }
